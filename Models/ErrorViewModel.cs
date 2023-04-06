81860d55ca1550659c6819ce421be603c9d1c4f6
Mgarcia456 <Asvp_miguel36@icloud.com>
2023-04-06 16:29:50 -0400

    Survey_with_validations

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CS8618
+namespace DojoSurvey.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
